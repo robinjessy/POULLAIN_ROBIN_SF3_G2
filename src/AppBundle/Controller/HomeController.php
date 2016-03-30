37f39ebabcd5ec8e8b5dc8e7d879e95cdc53f858
Jessy <robinjessyiim@gmail.com>
2016-03-30 14:59:09 +0200

    Ajout HomePage

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return new Response('HomePage');
+    }
+}
