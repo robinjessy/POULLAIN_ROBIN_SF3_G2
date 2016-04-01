cf92737a5d49e513960a7004a244dc38e1ad153e
Jessy <robinjessyiim@gmail.com>
2016-04-01 11:43:05 +0200

    Ajout fonctionnalité Add Article qui Marche de feu de dieu !!!

--- src/AppBundle/Controller/Article/ArticleController.php
+++ src/AppBundle/Controller/Article/ArticleController.php
@@ -1,7 +1,14 @@
 <?php
 
 namespace AppBundle\Controller\Article;
+
+use AppBundle\Entity\Article\Article;
+use AppBundle\Entity\Article\PostType;
+use AppBundle\Form\addArticle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,28 +17,81 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends Controller
 {
     /**
-     * @Route("/list", name="_article")
+     * @Route("/add", name="_article")
      */
-    public function listAction()
+    public function newAction(Request $request)
     {
-        $articleList = [
-            [
-                'id' => 2,
-                'name' => 'Symfony2'
-            ],
-            [
-                'id' => 5,
-                'name' => 'Wordpress'
-            ],
-            [
-                'id' => 9,
-                'name' => 'Laravel'
-            ],
-        ];
+        $article = new Article();
 
-        return $this->render('AppBundle:Article:index.html.twig', [
-            'articleList' => $articleList,
-        ]);
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('author', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Sauvegarder'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //Get Data
+            $title = $form['title']->getData();
+            $content = $form['content']->getData();
+            $author = $form['author']->getData();
+
+            $now = new\DateTime('now');
+
+            $article->setTitle($title);
+            $article->setContent($content);
+            $article->setAuthor($author);
+            $article->setCreatedAt($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em ->persist($article);
+            $em ->flush();
+
+            $this->addFlash(
+                'notice',
+                'article added'
+            );
+
+            return $this->redirectToRoute('_welcome');
+
+        }
+
+
+        return $this->render('AppBundle:Article:addArticle.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+
+//            // create a task and give it some dummy data for this example
+//            $article = new Article();
+//            $article->setTitle('title');
+//            $article->setContent('content');
+//
+//
+//            $form = $this->createFormBuilder($article)
+//                ->add('title', TextType::class)
+//                ->add('content', TextareaType::class)
+//                ->add('save', SubmitType::class, array('label' => 'Create Article'))
+//                ->add('saveAndAdd', SubmitType::class, array('label' => 'Save and Add'))
+//                ->getForm();
+//
+//            $form->handleRequest($request);
+//
+//            if ($form->isSubmitted() && $form->isValid()) {
+//                // ... perform some action, such as saving the task to the database
+//
+//                $nextAction = $form->get('saveAndAdd')->isClicked()
+//                    ? 'article_new'
+//                    : 'article_success';
+//
+//                return $this->redirectToRoute($nextAction);
+//            }
+//
+//            return $this->render('AppBundle:Article:addArticle.html.twig', array(
+//                'form' => $form->createView(),
+//            ));
     }
 
     /**
